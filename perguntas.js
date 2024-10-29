a462c388a93dd73204ad05e8b03ed4bc9b2b5f8b
Rafaeldugrau <00001089045803sp@al.educacao.sp.gov.br>
2024-10-29 11:35:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fisíca',
+    ' Um termômetro digital, localizado em uma praça na Inglaterra, marca a temperatura de 10,4 ºF. Essa temperatura, na escala Celsius, corresponde a ?',
+    ' - 12 ºC'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a é o maior estado do ESTADOS UNIDOS?',
+    'o maior estado é o alasca'
+)
+
+criaCartao(
+    'MATEMÁTICA',
+    'quais são as raizes da segunte equação x²+6x+5=0?',
+    'As raizes da euqação so -1 e 5'
+)
+
+criaCartao(
+    'Quimíca',
+    ' O octano é um dos principais constituintes da gasolina, que é uma mistura de hidrocarbonetos. A fórmula molecular do octano é ?',
+    'a fórmula  molecular é C8H18'
+)
